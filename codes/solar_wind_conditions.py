591a8fc9663f671970a3aa668b8f76c9a5bfc001
Ramiz Qudsi <qudsiramiz@gmail.com>
2022-06-06 18:04:58 -0400

    minor edits

--- codes/solar_wind_conditions.py
+++ codes/solar_wind_conditions.py
@@ -1,3 +1,4 @@
+from itertools import cycle
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,9 +24,37 @@ df['dynamic_pressure'] = 1.6726e-6 * 1.15 * df.np * df.vp_m**2
 
 # df_omni = df.resample('4W').median()
 df_omni = df.rolling(window='28D', min_periods=100).median()
+t_omni = (pd.Series(df_omni.index) - datetime.datetime(
+          1970, 1, 1, 0, 0, 0, 0, pytz.UTC)).dt.total_seconds()
 
-t_omni = (pd.Series(df_omni.index) -
-          datetime.datetime(1970, 1, 1, 0, 0, 0, 0, pytz.UTC)).dt.total_seconds()
+# Read the solar cycle list
+solar_cycle_list = pd.read_csv('../data/solar_cycle_list.csv')
+
+# Select data from df corresponding to the time interval of 1963-01-01 to 1964-01-01
+df_sc_20 = df[(df.index >= '1963-01-01 00:00:00') & (df.index <= '1964-01-01 00:00:00')]
+
+# Select the data from omni df corresponding to 1 year before the 24th solar cyle maximum 
+# and 1 year after the 24th solar cycle maximum
+df_sc_20_omni = df_omni[(df_omni.index >= solar_cycle_list.iloc[23].start_time) &
+                        (df_omni.index <= solar_cycle_list.iloc[23].end_time)]
+
+# Slect the data from omni df corresponding to 1 year before each solar cycle maximum and 1 year
+# after each solar cycle maximum
+df_sc_list = []
+for i in range(len(solar_cycle_list)):
+    df_sc_list.append(df_omni[(df_omni.index >= solar_cycle_list.iloc[i].start_time) &
+                              (df_omni.index <= solar_cycle_list.iloc[i].end_time)])
+
+# plot a histogram of np for each solar cycle
+fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+for i in range(len(solar_cycle_list)):
+    ax.hist(df_sc_list[i].np.dropna(), bins=100, label=solar_cycle_list.iloc[i].name)
+ax.set_xlabel('$n_p$ [$10^{19}$m$^{-3}$]')
+ax.set_ylabel('Frequency')
+ax.set_title('Histogram of $n_p$ for each solar cycle')
+ax.legend()
+plt.savefig('../figures/hist_np_sc.png', dpi=300)
+plt.show()
 
 # try :
 #     t_sc_unix = ((df.index - pd.datetime(1970, 1, 1, 0, 0, 0, 0, pytz.UTC)).total_seconds())
