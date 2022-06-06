7f143f8ed841465b939d6f8d4546a39e1569cde0
Ramiz Qudsi <qudsiramiz@gmail.com>
2022-06-06 18:21:28 -0400

    minor edits

--- codes/solar_wind_conditions.py
+++ codes/solar_wind_conditions.py
@@ -28,32 +28,46 @@ t_omni = (pd.Series(df_omni.index) - datetime.datetime(
           1970, 1, 1, 0, 0, 0, 0, pytz.UTC)).dt.total_seconds()
 
 # Read the solar cycle list
-solar_cycle_list = pd.read_csv('../data/solar_cycle_list.csv')
+df_sc = pd.read_csv('../data/solar_cycle_list.csv')
+
+year_list = []
+month_list = []
+for i in range(len(df_sc)):
+    try:
+        year_list.append(df_sc.Maximum[i].split('-')[0])
+        month_list.append(df_sc.Maximum[i].split('-')[1])
+    except:
+        pass
+
+# Get the datetime based on the year_list and month_list
+dt_list = []
+for i in range(len(year_list)):
+    dt_list.append(datetime.datetime(int(year_list[i]), int(month_list[i]), 1, 0, 0, 0, 0, pytz.UTC))
+
+# Compute the time 1 year prior to the solar cycle maximum and the time 1 year after the solar cycle
+# maximum and add it to the solar cycle list
+df_sc['t_1yr_prior'] = np.array(dt_list) - pd.to_timedelta(365, unit='d')
+df_sc['t_1yr_after'] = np.array(dt_list) + pd.to_timedelta(365, unit='d')
 
 # Select data from df corresponding to the time interval of 1963-01-01 to 1964-01-01
 df_sc_20 = df[(df.index >= '1963-01-01 00:00:00') & (df.index <= '1964-01-01 00:00:00')]
 
-# Select the data from omni df corresponding to 1 year before the 24th solar cyle maximum 
-# and 1 year after the 24th solar cycle maximum
-df_sc_20_omni = df_omni[(df_omni.index >= solar_cycle_list.iloc[23].start_time) &
-                        (df_omni.index <= solar_cycle_list.iloc[23].end_time)]
-
 # Slect the data from omni df corresponding to 1 year before each solar cycle maximum and 1 year
 # after each solar cycle maximum
 df_sc_list = []
-for i in range(len(solar_cycle_list)):
-    df_sc_list.append(df_omni[(df_omni.index >= solar_cycle_list.iloc[i].start_time) &
-                              (df_omni.index <= solar_cycle_list.iloc[i].end_time)])
+for i in range(len(df_sc)):
+    df_sc_list.append(df_omni[(df_omni.index >= df_sc.iloc[i].t_1yr_prior) &
+                              (df_omni.index <= df_sc.iloc[i].t_1yr_after)])
 
-# plot a histogram of np for each solar cycle
-fig, ax = plt.subplots(1, 1, figsize=(10, 6))
-for i in range(len(solar_cycle_list)):
-    ax.hist(df_sc_list[i].np.dropna(), bins=100, label=solar_cycle_list.iloc[i].name)
+# Make a histogram of np for each solar cycle
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+for i in range(len(df_sc_list)):
+    ax.hist(df_sc_list[i].np, bins=100, label=df_sc.Maximum[i])
 ax.set_xlabel('$n_p$ [$10^{19}$m$^{-3}$]')
-ax.set_ylabel('Frequency')
+ax.set_ylabel('Count')
 ax.set_title('Histogram of $n_p$ for each solar cycle')
 ax.legend()
-plt.savefig('../figures/hist_np_sc.png', dpi=300)
+plt.savefig('../figures/hist_np_solar_cycle.png')
 plt.show()
 
 # try :
